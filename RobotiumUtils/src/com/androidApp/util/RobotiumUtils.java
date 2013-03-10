2e5c1e49d5dadcbc560f7373898508998ae07e5b
Matthew Reynolds <mreynolds@redfoundry.com>
2013-03-10 11:36:57 -0700

    support for showing and hiding the IME

--- RobotiumUtils/src/com/androidApp/util/RobotiumUtils.java
+++ RobotiumUtils/src/com/androidApp/util/RobotiumUtils.java
@@ -5,9 +5,15 @@ import com.jayway.android.robotium.solo.Solo;
 
 import junit.framework.TestCase;
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Rect;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ListView;
+import android.widget.ScrollView;
 
 
 public class RobotiumUtils {
@@ -45,7 +51,7 @@ public class RobotiumUtils {
 		android.widget.ListView listView = (android.widget.ListView) solo.getView(android.widget.ListView.class, listViewIndex);
 		int waitMsec = VIEW_TIMEOUT_MSEC;
 		android.view.View listViewItem = null; 
-		while ((listView == null) && (waitMsec > 0)) {
+		while ((listViewItem == null) && (waitMsec > 0)) {
 			listViewItem = RobotiumUtils.getListViewItem(listView, itemIndex);
 			if (listViewItem != null) {
 				if (solo.waitForView(listViewItem)) {
@@ -133,5 +139,162 @@ public class RobotiumUtils {
 			}
 		}
 		return false;		
+	}
+	
+
+	/**
+	 * get the leastmost scrolling container for this view: NOTE: this may not be the container we're looking for,
+	 * since the view may be off the screen horizontally, and the container may only scroll vertically.
+	 * @param v
+	 * @return
+	 */
+	public static ScrollView getScrollingContainer(View v) {
+		ViewParent vp = v.getParent();
+		while (vp != null) {
+			if (vp instanceof ScrollView) {
+				return (ScrollView) vp;
+			}
+			vp = vp.getParent();
+			if (vp.getParent() == null) {
+				return null;
+			}
+		}
+		return null;
+	}
+	
+	/** 
+	 * return the rectangle of a view relative to its ancestor
+	 */
+	public static Rect getRelativeRect(ScrollView svAncestor, View view) {
+		Rect r = new Rect(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
+		ViewParent vp = view.getParent();
+		while (vp != svAncestor) {
+			r.offset(((View) vp).getLeft(), ((View) vp).getTop());
+			vp = vp.getParent();
+		}
+		r.offset(-svAncestor.getScrollX(), -svAncestor.getScrollY());
+		return r;
+	}
+	
+	/**
+	 * call on the activity ui thread to scroll a view until it's visible, so we can click or do something to it
+	 */
+	public class ScrollViewRunnable implements Runnable {
+		protected View mView;
+		protected int mXScroll;
+		protected int mYScroll;
+		
+		public ScrollViewRunnable(View view, int xScroll, int yScroll) {
+			mView = view;
+			mXScroll = xScroll;
+			mYScroll = yScroll;
+		}
+		public void run() {
+			mView.scrollBy(mXScroll, mYScroll);
+		}
+	}
+
+	/**
+	 * scroll the ancestor of the view until the view is fully visible.
+	 * @param v view to scroll into view.
+	 */
+	public static void scrollToViewVisible(Solo solo, View v) {
+		ScrollView scrollingContainer = RobotiumUtils.getScrollingContainer(v);
+		if (scrollingContainer != null) {
+			Rect r = RobotiumUtils.getRelativeRect(scrollingContainer, v);
+			int scrollVertical = 0;
+			int scrollHorizontal = 0;
+			if (r.top < 0) {
+				scrollVertical = r.top;
+			} else if (r.bottom > scrollingContainer.getMeasuredHeight()) {
+				scrollVertical = r.bottom - scrollingContainer.getMeasuredHeight();
+			}
+			if (r.left < 0) {
+				scrollHorizontal = r.left;
+			} else if (r.right > scrollingContainer.getMeasuredWidth()) {
+				scrollHorizontal = r.right - scrollingContainer.getMeasuredWidth();
+			}
+			if ((scrollVertical != 0) || (scrollHorizontal != 0)) {
+				RobotiumUtils utils = new RobotiumUtils();
+				Runnable runnable = utils.new ScrollViewRunnable(scrollingContainer, scrollHorizontal, scrollVertical);
+				RobotiumUtils.blockOnRunOnUiThread(solo.getCurrentActivity(), runnable, VIEW_TIMEOUT_MSEC);
+			}
+		}
+	}
+	
+	/**
+	 * if we want to run anything in the application, we have to use Activity.runOnUiThread(), but it returns immediately, and we then
+	 * probably send/expect something with the application immediately after, creating a recipe for a race condition.  Instead, we create
+	 * a wrapper around the runnable, which does a wait object, then a notify when the runnable has completed
+	 * @param activity to call runOnUiThread() 
+	 * @param runnable our intended victim
+	 * @param timeoutMsec time to wait before giving up
+	 */
+	public static void blockOnRunOnUiThread(Activity activity, Runnable runnable, long timeoutMsec) {
+		Object waitObject = new Object();
+		RobotiumUtils utils = new RobotiumUtils();
+		Runnable waitRunnable = utils.new BlockOnRunnable(activity, runnable, waitObject);
+		activity.runOnUiThread(waitRunnable);
+		try {
+			synchronized(waitObject) {
+				waitObject.wait(timeoutMsec);
+			}
+		} catch (InterruptedException iex) {	
+		}
+	}
+	
+	/**
+	 * wrapper for runnable which sends a notify() on completion
+	 */
+	public class BlockOnRunnable implements Runnable {
+		protected Activity  mActivity;
+		protected Runnable 	mRunnable;
+		protected Object	mWaitObject;
+		
+		public BlockOnRunnable(Activity activity, Runnable runnable, Object waitObject) {
+			mActivity = activity;
+			mRunnable = runnable;
+			mWaitObject = waitObject;
+		}
+		
+		public void run() {
+			mRunnable.run();
+			synchronized(mWaitObject) {
+				mWaitObject.notify();
+			}
+		}
+	}
+	
+	/**
+	 * unfortunately, views are often scrolled off the screen, so we often need to scroll them back to 
+	 * click on them.
+	 * @param solo handle to robotium
+	 * @param v view to click on
+	 */
+	public static void clickOnViewAndScrollIfNeeded(Solo solo, View v) {
+		RobotiumUtils.scrollToViewVisible(solo, v);
+		solo.clickOnView(v, true);
+	}
+	
+	/** 
+	 * sending the back key while the IME is up hides the IME, rather than exiting the activity, so
+	 * things have a tendency to get out of sync.  We detect show and hide IME events and perform
+	 * them explicitly
+	 * @param v view that the IME is being shown for.
+	 */
+	public static void showIME(View v) {
+        InputMethodManager imm = (InputMethodManager)v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(v, 0);
+		
+	}
+	
+	/**
+	 * see comment above.
+	 * @param v
+	 */
+	public static void hideIME(View v) {
+        InputMethodManager imm = (InputMethodManager)v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
 	}
 }
